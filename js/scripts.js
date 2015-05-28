f852f2764f6930c2d325a6e7ca7ffba5f6630353
David Garber & Jennifer Bolanos <student@epicodus.com>
2015-05-28 11:21:38 -0700

    return

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var romanNumeral = function(number) {
+number = 1
+var symbol = "I"
+  if(number === 1) {
+    return symbol;
+  }
+};
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe("romanNumeral", function() {
+  it("will return I for the number 1", function() {
+    expect(romanNumeral(1)).to.equal("I");
+  });
+});
