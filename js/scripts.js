80162200f4a821b25def4ea111fbcf81df92c9f1
David Garber & Jennifer Bolanos <student@epicodus.com>
2015-05-28 15:03:53 -0700

    convert any number between 9 and 39 to roman numerals.

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,27 @@ var numeralV = function(number) {
 
   return romanNumeral.join("");
 };
+
+var numeralX = function(number) {
+  var romanNumeral = [];
+
+  if(number === 9) {
+    romanNumeral.push("IX");
+  } else {
+    while (number >= 10) {
+      romanNumeral.push("X");
+      number -= 10;
+    }
+    if(number === 9) {
+      romanNumeral.push("IX");
+    }else if (number === 5) {
+      romanNumeral.push("V");
+    }else if (number < 4) {
+      romanNumeral.push(numeralI(number));
+    }else  {
+      romanNumeral.push(numeralV(number));
+    }
+  }
+
+  return romanNumeral.join("");
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,17 @@
 describe("numeralI", function() {
-  it("will return I for the number 1", function() {
+  it("converts any number between 1 and 3 to roman numeral", function() {
     expect(numeralI(1)).to.eql("I");
   });
 });
 
 describe("numeralV", function() {
-  it("will return V for the number 5", function() {
+  it("converts any number between 4 and 8 to roman numeral", function() {
     expect(numeralV(5)).to.equal("V");
+  });
+});
+
+describe("numeralX", function() {
+  it("converts any number between 9 and 39 to roman numeral", function() {
+    expect(numeralX(22)).to.equal("XXII");
   });
 });
