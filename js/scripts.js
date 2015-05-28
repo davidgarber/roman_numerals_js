2628efe22f187284b8dc9d01047ef396ac771f65
David Garber & Jennifer Bolanos <student@epicodus.com>
2015-05-28 14:31:41 -0700

    will return 'V' for the number 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
 // var romanNumeral = function(number) {
-var belowFour = function(number) {
+var numeralI = function(number) {
     var romanNumeral = "";
     for(var i = 1; i <= number; i++){
       romanNumeral += "I";
     }
     return romanNumeral;
+};
+
+var numeralV = function(number) {
+  var romanNumeral = [];
+
+  if(number === 4) {
+    romanNumeral.push("IV");
+  } else {
+    romanNumeral.push("V");
+    var newNum = number - 5;
+    romanNumeral.push(numeralI(newNum));
+  }
+
+  return romanNumeral.join("");
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,11 @@
-describe("belowFour", function() {
+describe("numeralI", function() {
   it("will return I for the number 1", function() {
-    expect(belowFour(1)).to.eql("I");
+    expect(numeralI(1)).to.eql("I");
+  });
+});
+
+describe("numeralV", function() {
+  it("will return V for the number 5", function() {
+    expect(numeralV(5)).to.equal("V");
   });
 });
